61a4c3c791f8c5c60d0c603958cbda5947b84362
Espebaum/gyopark <vocops5000@gmail.com>
2024-05-27 21:47:58 +0900

    fixed: search box

--- _plugins/generate_posts_json.rb
+++ _plugins/generate_posts_json.rb
@@ -17,19 +17,15 @@ module Jekyll
             image: post.data['image']
           }
         end
-      end.compact  # `compact`를 사용하여 `nil` 값을 제거
+      end.compact 
 
-      # 루트 디렉토리 경로를 동적으로 가져옴
       root_output_file = File.join(site.source, 'posts.json')
       site_output_file = File.join(site.dest, 'posts.json')
 
-      # 루트 디렉토리에 JSON 파일 생성
       File.write(root_output_file, JSON.pretty_generate(posts))
 
-      # _site 디렉토리에 JSON 파일 생성
       File.write(site_output_file, JSON.pretty_generate(posts))
 
-      # 파일 삭제 방지
       site.keep_files ||= []
       site.keep_files << 'posts.json'
     end
